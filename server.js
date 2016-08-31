035caffe52f9306ad29a738f467e9a8bd62b4da4
Gianmaria Leoni <gianmaria.leoni@gmail.com>
2016-08-31 14:08:30 +0200

    /schema

--- server.js
+++ server.js
@@ -4,20 +4,62 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-var json = {via: "", data: []};
 var output;
+var url = 'http://www.gruppohera.it/statico/bologna/db_pulizia_strade.php';
 
 app.set('port', (process.env.PORT || 5000));
 
+//get schema
+app.get('/schema', function(req, res){
+
+  request(url, function(error, response, html){
+    if(!error){
+      var json =[];
+      var $ = cheerio.load(html);
+
+      $("select#via").filter(function(){
+        $(this).children().slice(1).each(function(){
+
+          id = $(this).attr("value");
+          via = $(this).text().trim();
+
+          json.push({
+            id: id,
+            via: via,
+          });
+        });
+      })
+
+      output = req.query.pretty ? JSON.stringify(json, null, 4) : JSON.stringify(json);
+    }
+
+    // Print file
+    if (req.query.file) {
+      fs.writeFile('output.json', output, function(err){
+        console.log('File successfully written! - Check your project directory for the output.json file');
+      })
+    }
+
+    // Return message to browser
+    res.setHeader('Content-Type', 'application/json');
+    res.send(output);
+  })
+
+})
+
+//get by id
 app.get('/via/:id', function(req, res){
 
-  url = 'http://www.gruppohera.it/statico/bologna/db_pulizia_strade.php?via=' + req.params.id;
+  if (req.params.id) {
+    url += '?via=' + req.params.id;
+  }
 
   //?pretty=1 or ?pretty=true --> pretty json
   //?file=1   or ?file=true     --> file output
 
   request(url, function(error, response, html){
     if(!error){
+      var json = {via: "", data: []};
       var $ = cheerio.load(html);
       json = {via: "", data: []};
 
@@ -58,5 +100,5 @@ app.get('/via/:id', function(req, res){
 // exports = module.exports = app;
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'), '. Open browser in http://localhost:5000');
+})
